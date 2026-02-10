ee0abacc22404db91729c64d52cd20f207ea65c2
Andreas Geibert <ageibert@treehighroots.de>
2026-02-10 19:14:53 +0100

    refactors all mockup parts

--- advantago-location-finder/includes/Activator.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace AdvantagoLocationFinder;
-
-class Activator {
-    public static function activate() {
-        // Activation logic
-        flush_rewrite_rules();
-    }
-}
--- advantago-location-finder/includes/Deactivator.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace AdvantagoLocationFinder;
-
-class Deactivator {
-    public static function deactivate() {
-        // Deactivation logic
-        flush_rewrite_rules();
-    }
-}
